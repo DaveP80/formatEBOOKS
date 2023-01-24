dbfd9c15ec8cc529c502238019102b9204877239
davep80 <davidpaq1@gmail.com>
2023-01-24 15:42:42 -0500

    fix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import fitz
 import re
 from re import search
 from pathlib import Path
-doc = fitz.open('./Konrath.pdf')
+doc = fitz.open('./EJ.pdf')
 toc = doc.get_toc()
 #list of nums, begin page and end page
 pagelist = []
@@ -40,50 +40,66 @@ for i, t in enumerate(toc[:len(toc)-1]):
 
     if count==len(substring):
         headerlist.append(t[1])
-#print table of contents
-#print(toc)
+
 print(headerlist)
 print(pagelist)
-print(notpagelist)
 
 with Path('newdocument.text').open(mode='w') as output_file_3:
-    
-    for i, page in enumerate(doc):
-        if i in range(pagelist[0], pagelist[-1:][0]):
+    count = 0
+    check = False
+    for i in range(len(notpagelist)):
+        if notpagelist[i]<pagelist[-1:][0]:
+            count += 1
+    if count<len(notpagelist):
+        check = True
+    if count==len(notpagelist):
+        innercount = 0
+        checklist = pagelist[-10:]
+        for r in range(len(checklist)):
+            if notpagelist[-1:][0]<checklist[r]:
+                innercount += 1
+        if innercount==len(checklist):
+            notpagelist= []
+
+    if notpagelist:
+        if not check:
+            for i in range(len(notpagelist)):
+                if notpagelist[i]<pagelist[-1:][0]:
+                    if notpagelist[i]>pagelist[0]:
+                        for j in range(len(pagelist)):
+                            if pagelist[j]>notpagelist[i]:
+                                #if we have a good not page list
+                                pagelist.remove(pagelist[j])
+
+    print(pagelist)
+    print(notpagelist)
+    for p, page in enumerate(doc):
+        if p in range(pagelist[0], pagelist[-1:][0]):
             content = page.get_text()
             output_file_3.write(content)      
         else:
-            if i in range(pagelist[-1:][0]+1, notpagelist[-1:][0]):
-                content = page.get_text()
-                count = 0
-                for s in range(len(substring)):
-                    if not re.search(substring[s], content[0:25].lower()):
-                        count += 1
-                if count==len(substring):
-                    output_file_3.write(content)
+            if notpagelist:
+                if p in range(pagelist[-1:][0]+1, notpagelist[-1:][0]):
+                    content = page.get_text()
+                    count = 0
+                    for z in range(len(notpagelist)):
+                        if notpagelist[z]>pagelist[-1:][0]:
+                            if p==notpagelist[z]:
+                                quit()
+                    for s in range(len(substring)):
+                        if not re.search(substring[s], content[0:25].lower()):
+                            count += 1
+                    if count==len(substring):
+                        output_file_3.write(content)
 
-            if i>notpagelist[-1:][0]:
-                if i>pagelist[-1:][0]:
-                        content = page.get_text()
-                        count = 0
-                        if i in notpagelist:
-                            quit()
-                        else:
-                            count = 0
-                            for s in range(len(substring)):
-                                count += 1
-                                if not re.search(substring[s], content[0:25].lower()):
-                                    count += 1
-                            if count==len(substring):
-                                output_file_3.write(content)
-
-        
-        # elif i in range(pagelist[0], pagelist[0]+1):
-        #     content = page.get_text()
-        #     count = 0
-        #     for s in range(len(substring)):
-        #         if not re.search(substring[s], content[0:25].lower()):
-        #             count += 1
-
-        #     if count==len(substring):
-        #         output_file_3.write(content)
+            if not notpagelist:
+                if p in range(pagelist[-1:][0]+1, doc.page_count):
+                    content = page.get_text()
+                    count = 0
+                    for s in range(len(substring)):
+                        if not re.search(substring[s], content[0:25].lower()):
+                            count += 1
+                    if count>0:
+                        quit()
+                    if count==len(substring):
+                        output_file_3.write(content)
