354cf1379a689582389e232de09c2d1df45e28ba
davep80 <davidpaq1@gmail.com>
2023-01-24 03:43:11 -0500

    push

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 import fitz
 import re
 from re import search
-#print(fitz.__doc__)
-#doc = fitz.open('./Konrath.pdf')
-doc = fitz.open('./SEHS.pdf')
+from pathlib import Path
+doc = fitz.open('./Konrath.pdf')
 toc = doc.get_toc()
-#print(toc)
-import os
-import collections
-#list of 2 nums, begin page and end page
+#list of nums, begin page and end page
 pagelist = []
 #list of page headers
 headerlist = []
-#print(toc)
 
 substring = ['half title','title','title page','about the author','copyright','contents','author','introduction',
-'references','glossary','index','notes','key terms','conclusions','appendix','appendix:','foreword','biography','preface',
+'references','glossary','index','notes','key terms','conclusions','appendix','appendix:','foreword','biography',
+'dedication','disclaimers','preface','safety notices','number of printings'
 'acknowledgments','afterword','postscript','prologue','bibliography']
+
 for i, t in enumerate(toc[:len(toc)-2]):
     count = 0
     for s in range(len(substring)):
@@ -37,25 +34,32 @@ for i, t in enumerate(toc[:len(toc)-2]):
 
     if count==len(substring):
         headerlist.append(t[1])
-        
-# for i, t in enumerate(toc[:len(toc)-2]):
-#     if t[1].lower() not in substring:
-#         headerlist.append(t[1])
-#pagelist.append(doc.page_count)
-
-print(headerlist)
+#print table of contents
+print(toc)
 print(pagelist)
 
-for i, page in enumerate(doc):
-    if i in range(pagelist[0], pagelist[-1:][0]+2):
-        content = page.get_text()
-        if i>pagelist[-1:][0]:
+with Path('newdocument.text').open(mode='w') as output_file_3:
+    
+    for i, page in enumerate(doc):
+        if i in range(pagelist[1], pagelist[-1:][0]+2):
+            content = page.get_text()
+            if i>pagelist[-1:][0]:
+                count = 0
+                for s in range(len(substring)):
+                    if not re.search(substring[s], content[0:25].lower()):
+                        count += 1
+
+                if count==len(substring):
+                    output_file_3.write(content)
+            else:
+                output_file_3.write(content)
+        
+        elif i in range(pagelist[0], pagelist[0]+1):
+            content = page.get_text()
             count = 0
             for s in range(len(substring)):
                 if not re.search(substring[s], content[0:25].lower()):
                     count += 1
 
             if count==len(substring):
-                print(content)
-        else:
-            print(content)
+                output_file_3.write(content)
